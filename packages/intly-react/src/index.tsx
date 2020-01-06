fa60fc175bb2b364cb677ac895a9c4d24eb2c443
Yusuke Otsuka <otsuka_yusuke@cyberagent.co.jp>
2020-01-06 13:36:32 +0900

    fix: memory leak in SSR

--- packages/intly-react/src/index.tsx
+++ packages/intly-react/src/index.tsx
@@ -5,18 +5,26 @@ export interface Props {
   children: React.ReactNode;
 }
 
-export function createIntlyHandler<T extends AbstractDictionary>(intly: Intly<T>) {
-  return class IntlyHandler extends React.PureComponent<Props> {
-    public constructor(props: Props) {
-      super(props);
+// NOTE: this declaration is needed to avoid typescript's compile error (TS4094)
+// https://github.com/microsoft/TypeScript/issues/30355
+class IntlyHandler extends React.PureComponent<Props> {}
 
-      intly.on('languageChanged', () => {
-        this.forceUpdate();
-      });
+export function createIntlyHandler<T extends AbstractDictionary>(intly: Intly<T>): typeof IntlyHandler {
+  return class extends IntlyHandler {
+    public componentDidMount() {
+      intly.on('languageChanged', this.handleLanguageChanged);
+    }
+
+    public componentWillUnmount() {
+      intly.off('languageChanged', this.handleLanguageChanged);
     }
 
     public render() {
       return <React.Fragment key={intly.getLanguage()}>{this.props.children}</React.Fragment>;
+    }
+
+    private handleLanguageChanged = () => {
+      this.forceUpdate();
     }
   };
 }
